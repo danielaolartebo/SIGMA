175a13d1b318b5fa4d892ebcc33ff0f8bd367152
Daniela Olarte <danielaolarte123@gmail.com>
2024-11-06 18:58:59 -0500

    new filter added

--- src/Filters.js
+++ src/Filters.js
@@ -4,9 +4,12 @@ function Dropdown() {
     const [faculties, setFaculties] = useState([]); // State for Faculty options
     const [programs, setPrograms] = useState([]); // State for Program options
     const [subject, setSubject] = useState([]); // State for Subject options
+    const [state, setState] = useState([]); // State for State options
+
     const [selectedFaculty, setSelectedFaculty] = useState(""); // Selected Faculty
     const [selectedProgram, setSelectedProgram] = useState(""); // Selected Program
     const [selectedSubject, setSelectedSubject] = useState(""); // Selected Subject
+    const [selectedState, setSelectedState] = useState(""); // Selected State
 
     // Fetch Faculty options
     useEffect(() => {
@@ -65,6 +68,26 @@ function Dropdown() {
             .catch(error => console.error('Error fetching program data:', error));
     }, []);
 
+    // Fetch State options
+    useEffect(() => {
+        fetch('http://localhost:3000/State.json')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data.state) {
+                    setState(data.state);
+                } else {
+                    console.error("Program data format is incorrect.");
+                }
+            })
+            .catch(error => console.error('Error fetching program data:', error));
+    }, []);
+
+
     // Handle change for Faculty dropdown
     const handleFacultyChange = (event) => {
         setSelectedFaculty(event.target.value);
@@ -79,6 +102,12 @@ function Dropdown() {
      const handleSubjectChange = (event) => {
         setSelectedSubject(event.target.value);
     };
+
+    // Handle change for State dropdown
+     const handleStateChange = (event) => {
+        setSelectedState(event.target.value);
+    };
+
 
     return (
         <div className="filter">
@@ -121,6 +150,20 @@ function Dropdown() {
                     {subject.map(subject => (
                         <option key={subject.id} value={subject.id}>
                             {subject.name}
+                        </option>
+                    ))}
+                </select>
+
+                {/* State Dropdown */}
+                <select className="state"
+                    id="state-dropdown" 
+                    value={selectedState} 
+                    onChange={handleStateChange}
+                >
+                    <option value=""> Estado </option>
+                    {state.map(state => (
+                        <option key={state.id} value={state.id}>
+                            {state.name}
                         </option>
                     ))}
                 </select>
